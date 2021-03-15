668d50808fa52be80aa7c57967f8ce9a1cb575d2
mbdig3480 <77412306+mbdig3480@users.noreply.github.com>
2021-03-14 22:42:27 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,14 +7,26 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
     public float speed;
-    public Text score;
+    public Text winText;
+    public Text loseText;
+    public Text livesText;
+    public Text scoreText;
     private int scoreValue = 0;
+    private int livesValue = 3;
+    public AudioSource musicSource;
+    public AudioClip soundEffect;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        anim = GetComponent<Animator>();
+        scoreText.text = scoreValue.ToString();
+        setScoreText();
+        setLivesText();
+        winText.text = "";
+        loseText.text = "";
     }
 
     // Update is called once per frame
@@ -31,8 +43,15 @@ public class PlayerScript : MonoBehaviour
         if(collision.collider.tag == "Coin") 
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            setScoreText();
+        }
+
+        else if (collision.collider.tag == "Enemy")
+        {
+            livesValue = livesValue - 1;
+            Destroy(collision.collider.gameObject);
+            setLivesText();
         }
     }
 
@@ -45,5 +64,44 @@ public class PlayerScript : MonoBehaviour
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
         }
+    }
+
+    void setScoreText() {
+        scoreText.text="Score: " + scoreValue.ToString();
+
+        if (scoreValue==8) {
+            winText.text="You win! By Mallory Bronson";
+            DestroyGameObject();
+            musicSource.clip=soundEffect;
+            musicSource.Play();
+        }
+
+        if (scoreValue==4) 
+        {
+            transform.position = new Vector2(-10.85f, 0.5772f);
+            livesValue = 3;
+            livesText.text = "Lives: " +livesValue.ToString();
+        }
+    }
+
+    void setLivesText() {
+        livesText.text="Lives: " + livesValue.ToString();
+
+        if (livesValue==0) {
+            loseText.text="You lose! By Mallory Bronson";
+            DestroyGameObject();
+        }
+    }
+
+    void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+
+    void Animation () {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+           anim.SetInteger("State", 4);
+         }
     }
 }
